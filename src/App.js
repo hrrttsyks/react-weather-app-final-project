6fb566cd30f1d7ab0526766ada94a0b17ba2ca0a
hrrttsyks <140945519+hrrttsyks@users.noreply.github.com>
2024-01-03 14:50:06 +0000

    installed bootstrap into project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Harriett Sykes and is
-        <a
-          href="https://github.com/hrrttsyks/react-weather-app-final-project"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Harriett Sykes and is
+          <a
+            href="https://github.com/hrrttsyks/react-weather-app-final-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
