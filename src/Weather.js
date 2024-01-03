5ad5fced7c6728ee9cfa309dbaf4a1adca57844c
hrrttsyks <140945519+hrrttsyks@users.noreply.github.com>
2024-01-03 16:51:00 +0000

    Added CSS styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,19 @@ export default function Weather() {
         <li>Wednesday 14:58</li>
         <li>Light Rain</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="light rain"
-          ></img>
-          6°C | °F
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+              alt="light rain"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C | °F</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
