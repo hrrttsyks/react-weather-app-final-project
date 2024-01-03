09ddde29727a94129fb1aecb09d1da701bf52e10
hrrttsyks <140945519+hrrttsyks@users.noreply.github.com>
2024-01-03 14:26:20 +0000

    added link to git hub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Harriett Sykes and is
+        <a
+          href="https://github.com/hrrttsyks/react-weather-app-final-project"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
